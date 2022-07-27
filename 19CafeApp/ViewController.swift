787e6af68fa36797048f0b15afd098f362b8fce0
김성진 <zzrkdl@naver.com>
2022-07-27 22:12:37 +0900

    Initial Commit

---
+++ 19CafeApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  19CafeApp
+//
+//  Created by 김성진 on 2022/07/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
